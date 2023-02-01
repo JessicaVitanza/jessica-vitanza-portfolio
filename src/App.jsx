149a5d530350be53b70a8ece3cf08b6717f68f28
JessicaVitanza <jessicavitanzauidesign@gmail.com>
2023-02-01 21:48:37 +0100

    navbar icons commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,13 @@ function App() {
   return (
     <>
       <div className={styles.App}>
-          <Link to="/">Home</Link>
-          <Link to="aboutme">About me</Link>
-          <Link to="esperienze">Esperienze</Link>
-          <Link to="formazione">Formazione</Link>
-          <Link to="competenze">Competenze</Link>
-          <Link to="progetti">Progetti</Link>
-          <Link to="contatti">Contatti</Link>
+          <Link to="/"><img src="./home.gif" alt="home" /><p>Home</p></Link>
+          <Link to="aboutme"><img src="./girl.gif" alt="aboutme" /><p>About me</p></Link>
+          <Link to="esperienze"><img src="./work.gif" alt="esperienze" /><p>Esperienze</p></Link>
+          <Link to="formazione"><img src="./book.gif" alt="formazione" /><p>Formazione</p></Link>
+          <Link to="competenze"><img src="./cloud.gif" alt="competenze" /><p>Competenze</p></Link>
+          <Link to="progetti"><img src="./project.gif" alt="progetti" /><p>Progetti</p></Link>
+          <Link to="contatti"><img src="./mail.gif" alt="contatti" /><p>Contatti</p></Link>
       </div>
 
       <Outlet />
