72bd0ba9c0dc8ed14f5b07c95c80023de934e00d
JessicaVitanza <jessicavitanzauidesign@gmail.com>
2023-02-19 10:03:54 +0100

    add bootstrap carousel

--- src/routes/progetti/Progetti.jsx
+++ src/routes/progetti/Progetti.jsx
@@ -1,15 +1,61 @@
 import styles from "./progetti.module.scss";
+import React, { useState } from 'react';
+
 import 'animate.css';
+
+
+import {Carousel} from 'react-bootstrap';
+
 function Progetti() {
 
-  return ( 
+  const [index, setIndex] = useState(0);
 
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
 
+  return (
+    <div className={styles.Progetti}>
+     <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="./cloud.gif"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="./girl.gif"
+          alt="Second slide"
+        />
 
-  <div className={styles.Progetti} style={{ backgroundImage: `url("./green.svg")`}}>
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="./home.gif"
+          alt="Third slide"
+        />
 
-  </div>
-
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+    </div>
   );
 }
 
